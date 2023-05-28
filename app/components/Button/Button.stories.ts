c79a878b59bc47d0a5bbea59c564c38c81839718
Joseph Collicoat <jcollicoat@gmail.com>
2023-05-28 13:42:28 +1200

    Add basic button and story

---
+++ app/components/Button/Button.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+    title: "Button",
+    component: Button,
+    tags: ["autodocs"],
+    argTypes: {
+        onClick: {
+            table: {
+                disable: true,
+            },
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+    args: {
+        ariaLabel: "Button",
+        text: "Button",
+        onClick: () => alert("Button clicked!"),
+    },
+};
---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,65 @@
+import classNames from "classnames";
+import Link from "next/link";
+import { FC } from "react";
+import styles from "./Button.module.scss";
+
+type ButtonStyles = "cta" | "primary" | "secondary" | "tertiary";
+
+interface BaseProps {
+    ariaLabel: string;
+    text: string;
+    style?: ButtonStyles;
+}
+
+interface PropsForButton extends BaseProps {
+    onClick: () => void;
+    link?: never;
+}
+
+interface PropsForLink extends BaseProps {
+    link: string;
+    onClick?: never;
+}
+
+type Props = PropsForButton | PropsForLink;
+
+export const Button: FC<Props> = ({
+    ariaLabel,
+    text,
+    style = "primary",
+    onClick,
+    link,
+}) => {
+    if (onClick) {
+        return (
+            <button
+                aria-label={ariaLabel}
+                className={classNames(styles.button, styles[style])}
+                onClick={onClick}
+                type="button"
+            >
+                {text}
+            </button>
+        );
+    }
+
+    return link.startsWith("https://") ? (
+        <a
+            aria-label={ariaLabel}
+            className={classNames(styles.button, styles[style])}
+            href={link}
+            rel="noopener noreferrer"
+            target="_blank"
+        >
+            {text}
+        </a>
+    ) : (
+        <Link
+            aria-label={ariaLabel}
+            className={classNames(styles.button, styles[style])}
+            href={link}
+        >
+            {text}
+        </Link>
+    );
+};
