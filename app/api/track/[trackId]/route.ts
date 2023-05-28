7f0fa887a6924666d214d3391837c79ae32e9825
Joseph Collicoat <jcollicoat@gmail.com>
2023-05-28 16:40:36 +1200

    Get a track via API

--- app/api/track/[trackId]/route.ts
+++ app/api/track/[trackId]/route.ts
@@ -1,7 +1,23 @@
-import { NextRequest } from "next/server";
+import { SpotifyTrack } from "@/library/spotify/tracks/types";
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+const spotifyEndpoint = "https://api.spotify.com/v1/tracks/";
 
 export async function GET(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-    const trackId = pathname.replace("/api/track/", "");
-    return new Response(`This is your track ID: ${trackId}`);
+    try {
+        const spotifyToken = request.headers.get("authorization");
+        const trackId = request.nextUrl.pathname.replace("/api/track/", "");
+        const spotifyTrack = await axios.get<SpotifyTrack>(
+            spotifyEndpoint + trackId,
+            {
+                headers: {
+                    Authorization: spotifyToken,
+                },
+            }
+        );
+        return NextResponse.json(spotifyTrack.data);
+    } catch (error) {
+        return new Response("Something went wrong");
+    }
 }
