bbe705d86b08f0026fd8be2dc57512342de32eea
Joseph Collicoat <jcollicoat@gmail.com>
2023-05-28 22:04:40 +1200

    Add basic track builder

---
+++ app/library/tracks/builders.ts
@@ -0,0 +1,43 @@
+import { SpotifyTrack } from "@/library/spotify/tracks/types";
+import { Track } from "@/library/tracks/types";
+
+const buildAlbumArtists = (
+    artists: SpotifyTrack["album"]["artists"]
+): Track["album"]["artists"] => {
+    return artists.map((artist) => ({
+        id: artist.id,
+        name: artist.name,
+    }));
+};
+
+const buildAlbum = (album: SpotifyTrack["album"]): Track["album"] => {
+    return {
+        albumType: album.album_type,
+        artists: buildAlbumArtists(album.artists),
+        id: album.id,
+        name: album.name,
+        totalTracks: album.total_tracks,
+    };
+};
+
+const buildArtists = (artists: SpotifyTrack["artists"]): Track["artists"] => {
+    return artists.map((artist) => ({
+        id: artist.id,
+        name: artist.name,
+    }));
+};
+
+export const buildSpotifyTrack = (track: SpotifyTrack): Track => {
+    const { duration_ms, explicit, id, name, popularity, preview_url } = track;
+
+    return {
+        album: buildAlbum(track.album),
+        artists: buildArtists(track.artists),
+        durationMs: duration_ms,
+        explicit: explicit,
+        id: id,
+        name: name,
+        popularity: popularity,
+        previewUrl: preview_url,
+    };
+};
