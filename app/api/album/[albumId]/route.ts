a294cc56e16240cb3698932fc63664f8f1dfd97f
Joseph Collicoat <jcollicoat@gmail.com>
2023-05-28 17:18:34 +1200

    Add album route and helper methods

--- app/api/album/[albumId]/route.ts
+++ app/api/album/[albumId]/route.ts
@@ -1,24 +1,24 @@
 import { getAccessToken } from "@/library/auth/helpers";
 import { createErrorResponse } from "@/library/generic/helpers";
-import { SpotifyTrack } from "@/library/spotify/tracks/types";
+import { SpotifyAlbum } from "@/library/spotify/albums/types";
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-const spotifyEndpoint = "https://api.spotify.com/v1/tracks/";
+const spotifyEndpoint = "https://api.spotify.com/v1/albums/";
 
 export async function GET(request: NextRequest) {
     try {
         const accessToken = getAccessToken(request);
-        const trackEndpoint = request.nextUrl.pathname.replace(
+        const albumEndpoint = request.nextUrl.pathname.replace(
             "/api/album/",
             spotifyEndpoint
         );
-        const spotifyTrack = await axios.get<SpotifyTrack>(trackEndpoint, {
+        const spotifyAlbum = await axios.get<SpotifyAlbum>(albumEndpoint, {
             headers: {
                 Authorization: accessToken,
             },
         });
-        return NextResponse.json(spotifyTrack.data);
+        return NextResponse.json(spotifyAlbum.data);
     } catch (error) {
         const { status, message } = createErrorResponse(error);
         return new Response(message, { status: status });
