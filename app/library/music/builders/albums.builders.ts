a6092725c3ed9ae9cd011dd04c6e185127e559a0
Joseph Collicoat <jcollicoat@gmail.com>
2023-06-05 16:23:32 +1200

    Album builder

--- app/library/music/builders/albums.builders.ts
+++ app/library/music/builders/albums.builders.ts
@@ -1,38 +1,22 @@
+import { builders } from '@music/builders';
 import { Album } from '@music/types/albums.types';
 import { SpotifyAlbum } from '@spotify/types/albums.types';
 
-const buildArtists = (artists: SpotifyAlbum['artists']): Album['artists'] =>
-    artists.map((artist) => ({
-        id: artist.id,
-        name: artist.name,
-    }));
-
-const buildTracks = (tracks: SpotifyAlbum['tracks']): Album['tracks'] =>
-    tracks?.items.map((track) => ({
-        artists: track.artists.map((artist) => ({
-            id: artist.id,
-            name: artist.name,
-        })),
-        durationMs: track.duration_ms,
-        explicit: track.explicit,
-        id: track.id,
-        name: track.name,
-        previewUrl: track.preview_url,
-    }));
-
 const buildAlbum = (album: SpotifyAlbum): Album => ({
     albumType: album.album_type,
-    artists: buildArtists(album.artists),
-    genres: album.genres,
+    artists: album.artists.map((artist) =>
+        builders.artists.buildArtist(artist)
+    ),
     id: album.id,
     images: album.images,
-    label: album.label,
     name: album.name,
-    popularity: album.popularity,
     releaseDate: album.release_date,
     releaseDatePrecision: album.release_date_precision,
     totalTracks: album.total_tracks,
-    tracks: buildTracks(album.tracks),
+    genres: album.genres,
+    label: album.label,
+    popularity: album.popularity,
+    tracks: album.tracks && builders.tracks.buildTracks(album.tracks.items),
 });
 
 const albums = { buildAlbum };
