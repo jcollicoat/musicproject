14e7384cc1a606b2f0d01dddfaca6c677109b48b
Joseph Collicoat <jcollicoat@gmail.com>
2023-06-05 16:26:58 +1200

    Artist builder

--- app/library/music/builders/artists.builders.ts
+++ app/library/music/builders/artists.builders.ts
@@ -2,11 +2,11 @@ import { Artist } from '@music/types/artists.types';
 import { SpotifyArtist } from '@spotify/types/artists.types';
 
 const buildArtist = (artist: SpotifyArtist): Artist => ({
-    followers: artist.followers.total,
-    genres: artist.genres,
     id: artist.id,
-    images: artist.images,
     name: artist.name,
+    followers: artist.followers?.total,
+    genres: artist.genres,
+    images: artist.images,
     popularity: artist.popularity,
 });
 
