240948bc64e8fc3a3ae516e0e7557ac1a2e4a183
Joseph Collicoat <jcollicoat@gmail.com>
2023-06-17 12:34:58 +1200

    Basic panel

---
+++ app/components/Panel/Panel.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@components/Button/Button';
+import { Panel } from './Panel';
+
+const meta: Meta<typeof Panel> = {
+    title: 'Panel',
+    component: Panel,
+    tags: ['autodocs'],
+    argTypes: {
+        children: {
+            control: { type: 'radio' },
+            options: ['Button', 'Secondary Button'],
+            mapping: {
+                Button: (
+                    <Button
+                        onClick={() => alert('Clicked')}
+                        ariaLabel="Button"
+                        text="Button"
+                    />
+                ),
+                'Secondary Button': (
+                    <Button
+                        onClick={() => alert('Clicked')}
+                        ariaLabel="Secondary Button"
+                        text="Secondary Button"
+                        style="secondary"
+                    />
+                ),
+            },
+        },
+        position: { table: { disable: true } },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Panel>;
+
+export const Primary: Story = {
+    args: {
+        children: 'Button',
+    },
+};
---
+++ app/components/Panel/Panel.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react';
+import styles from './Panel.module.scss';
+
+interface Props {
+    children: ReactNode;
+    position?: string;
+}
+
+export const Panel: FC<Props> = ({ children, position }) => {
+    return (
+        <section className={styles.panel} style={{ gridArea: position }}>
+            <div>{children}</div>
+        </section>
+    );
+};
