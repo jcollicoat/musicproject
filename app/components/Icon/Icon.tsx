ec4dcc6cb06984e05f3d0f59b8044d8c602aa50c
Joseph Collicoat <jcollicoat@gmail.com>
2023-06-26 18:43:15 +1200

    Code re-use in icons

--- app/components/Icon/Icon.tsx
+++ app/components/Icon/Icon.tsx
@@ -1,42 +1,57 @@
-import { FC, Suspense, lazy } from 'react';
+import { CSSProperties, FC, Suspense, lazy } from 'react';
 import styles from './Icon.module.scss';
 
 // Pulsing loading dots
 const Fallback: FC = () => (
     <svg
-        className={styles.fallback}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 14 14"
-        height="24"
-        width="24"
+        height="100%"
+        width="100%"
+        fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
     >
-        <path d="M4 7.25C3.86193 7.25 3.75 7.13807 3.75 7C3.75 6.86193 3.86193 6.75 4 6.75"></path>
-        <path d="M4 7.25C4.13807 7.25 4.25 7.13807 4.25 7C4.25 6.86193 4.13807 6.75 4 6.75"></path>
-        <path d="M7 7.25C6.86193 7.25 6.75 7.13807 6.75 7C6.75 6.86193 6.86193 6.75 7 6.75"></path>
-        <path d="M7 7.25C7.13807 7.25 7.25 7.13807 7.25 7C7.25 6.86193 7.13807 6.75 7 6.75"></path>
-        <path d="M10 7.25C9.86193 7.25 9.75 7.13807 9.75 7C9.75 6.86193 9.86193 6.75 10 6.75"></path>
-        <path d="M10 7.25C10.1381 7.25 10.25 7.13807 10.25 7C10.25 6.86193 10.1381 6.75 10 6.75"></path>
+        <g className={styles.fallback}>
+            <path d="M4 7.25C3.86193 7.25 3.75 7.13807 3.75 7C3.75 6.86193 3.86193 6.75 4 6.75"></path>
+            <path d="M4 7.25C4.13807 7.25 4.25 7.13807 4.25 7C4.25 6.86193 4.13807 6.75 4 6.75"></path>
+            <path d="M7 7.25C6.86193 7.25 6.75 7.13807 6.75 7C6.75 6.86193 6.86193 6.75 7 6.75"></path>
+            <path d="M7 7.25C7.13807 7.25 7.25 7.13807 7.25 7C7.25 6.86193 7.13807 6.75 7 6.75"></path>
+            <path d="M10 7.25C9.86193 7.25 9.75 7.13807 9.75 7C9.75 6.86193 9.86193 6.75 10 6.75"></path>
+            <path d="M10 7.25C10.1381 7.25 10.25 7.13807 10.25 7C10.25 6.86193 10.1381 6.75 10 6.75"></path>
+        </g>
     </svg>
 );
 
 type Icons = 'Close' | 'Heart' | 'Menu';
 
-interface Props {
+export interface IconProps {
     icon: Icons;
     isAlternate?: boolean;
+    size?: CSSProperties['fontSize'];
 }
 
-export const Icon: FC<Props> = ({ icon, isAlternate }) => {
+export const Icon: FC<IconProps> = ({ icon, isAlternate, size }) => {
     const Icon = lazy(() => import(`./icons/${icon}`));
 
     return (
-        <Suspense fallback={<Fallback />}>
-            <div className={styles.container}>
-                <Icon isAlternate={isAlternate} />
-            </div>
-        </Suspense>
+        <div className={styles.container} style={{ fontSize: size }}>
+            <svg
+                className={styles.container}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 14 14"
+                height="100%"
+                width="100%"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <Suspense fallback={<Fallback />}>
+                    <Icon isAlternate={isAlternate} />
+                </Suspense>
+            </svg>
+        </div>
     );
 };
--- app/components/Icon/icons/Heart.tsx
+++ app/components/Icon/icons/Heart.tsx
@@ -1,17 +1,7 @@
 import { FC } from 'react';
 
 const Heart: FC = () => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 14 14"
-        height="100%"
-        width="100%"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-    >
-        <path d="M7.004 12.383 1.53 7.424c-2.975-2.975 1.398-8.688 5.474-4.066 4.076-4.622 8.43 1.11 5.475 4.066l-5.475 4.959Z"></path>
-    </svg>
+    <path d="M7.004 12.383 1.53 7.424c-2.975-2.975 1.398-8.688 5.474-4.066 4.076-4.622 8.43 1.11 5.475 4.066l-5.475 4.959Z"></path>
 );
 
 export default Heart;
