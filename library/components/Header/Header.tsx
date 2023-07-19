8b9e79468d0886a07e68b12fd8f16e88cbb41e67
Joseph Collicoat <jcollicoat@gmail.com>
2023-07-19 15:39:08 +1200

    Remove scroll styles

--- library/components/Header/Header.tsx
+++ library/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 
 import classNames from 'classnames';
 import { FC } from 'react';
-import { useWindowScroll } from 'react-use';
 import { ButtonContainer } from '@components/Button/Button';
 import { useHeaderButtons } from '@hooks/useHeaderButtons';
 import { Track } from '@music/types/tracks.types';
@@ -22,17 +21,15 @@ const HeaderContent: FC<ContentProps> = ({
     headingElement: Heading,
     title,
     subtitle,
-}) => {
-    return (
-        <>
-            <div className={styles.titles}>
-                <Heading className={styles.title}>{title}</Heading>
-                {subtitle && <p>{subtitle}</p>}
-            </div>
-            <ButtonContainer buttons={useHeaderButtons()} />
-        </>
-    );
-};
+}) => (
+    <>
+        <div className={styles.titles}>
+            <Heading className={styles.title}>{title}</Heading>
+            {subtitle && <p>{subtitle}</p>}
+        </div>
+        <ButtonContainer buttons={useHeaderButtons()} />
+    </>
+);
 
 interface Props extends BaseProps {
     data?: Track;
@@ -44,32 +41,15 @@ export const Header: FC<Props> = ({
     subtitle,
     data,
     isSticky = true,
-}) => {
-    const { y } = useWindowScroll();
-    const isScrolled = y >= 100;
-
-    return (
-        <header
-            className={classNames(
-                styles.header,
-                isSticky && styles.isSticky,
-                isScrolled && styles.isScrolled,
-            )}
-        >
-            <div
-                className={classNames(
-                    styles.content,
-                    data && styles.hasData,
-                    isSticky && styles.isSticky,
-                )}
-            >
-                <HeaderContent
-                    headingElement={data ? 'span' : 'h1'}
-                    title={title}
-                    subtitle={subtitle}
-                />
-            </div>
-            {data && <HeaderTrack track={data} />}
-        </header>
-    );
-};
+}) => (
+    <header className={classNames(styles.header, isSticky && styles.isSticky)}>
+        <div className={classNames(styles.content, data && styles.hasData)}>
+            <HeaderContent
+                headingElement={data ? 'span' : 'h1'}
+                title={title}
+                subtitle={subtitle}
+            />
+        </div>
+        {data && <HeaderTrack track={data} />}
+    </header>
+);
