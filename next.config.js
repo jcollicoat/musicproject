2ecc1a6d7da936cf5dc27d0e9e37d4365fe100cd
Joseph Collicoat <jcollicoat@gmail.com>
2023-07-21 16:22:31 +1200

    Allowed image domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'i.scdn.co',
             },
+            {
+                protocol: 'https',
+                hostname: 'platform-lookaside.fbsbx.com',
+            },
         ],
     },
 };
