022db308bf7f4a5f0cec68d6c1e223cedaf2e030
Joseph Collicoat <jcollicoat@gmail.com>
2023-07-23 22:24:31 +1200

    Basic grid

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,5 +1,7 @@
 import { authGuard } from '@api/helpers';
 import { Header } from '@components/Header/Header';
+import { Panel } from '@components/Panel/Panel';
+import styles from './page.module.scss';
 
 export const metadata = {
     title: 'My Music',
@@ -7,5 +9,15 @@ export const metadata = {
 
 export default async function Page() {
     await authGuard();
-    return <Header subtitle="My Music" data="user" />;
+    return (
+        <>
+            <Header subtitle="My Music" data="user" />
+            <main className={styles.main}>
+                <Panel position="library">1</Panel>
+                <Panel position="recent">2</Panel>
+                <Panel position="artists">3</Panel>
+                <Panel position="albums">4</Panel>
+            </main>
+        </>
+    );
 }
