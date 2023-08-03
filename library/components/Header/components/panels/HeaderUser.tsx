52b70cc43078271da854de05f7ebf694f7524aa1
Joseph Collicoat <jcollicoat@gmail.com>
2023-08-04 00:48:43 +1200

    HeaderUser design

--- library/components/Header/components/panels/HeaderUser.tsx
+++ library/components/Header/components/panels/HeaderUser.tsx
@@ -16,39 +16,35 @@ export const HeaderUser: FC<Props> = ({ user, image }) => (
             <div className={styles.main}>
                 <h1 className={styles.title}>My Music</h1>
             </div>
-            <aside className={styles.side}>
-                <Panel element="div">
-                    <div className={styles.profile}>
-                        <div className={styles.details}>
-                            <Image
-                                src={user.images.large.url}
-                                alt={`${user.name}'s profile picture`}
-                                height={60}
-                                width={60}
-                                className={styles.image}
-                            />
-                            <span className={styles.name}>{user.name}</span>
+            <div className={styles.profile}>
+                <Image
+                    src={user.images.large.url}
+                    alt={`${user.name}'s profile picture`}
+                    height={60}
+                    width={60}
+                    className={styles.image}
+                />
+                <div className={styles.details}>
+                    <span className={styles.name}>{user.name}</span>
+                    <div className={styles.products}>
+                        <div className={styles.product}>
+                            <Icon icon="Heart" size="1rem" />
+                            {`${user.followers} Followers`}
                         </div>
-                        <div className={styles.products}>
-                            <div className={styles.product}>
-                                <Icon icon="Heart" size="1rem" />
-                                {`${user.followers} Followers`}
-                            </div>
-                            <div className={styles.product}>
-                                <Icon icon="Star" size="1rem" />
-                                {user.product.charAt(0).toUpperCase() +
-                                    user.product.slice(1)}
-                            </div>
-                            <div className={styles.product}>
-                                <Icon icon="Lock" size="1rem" />
-                                {user.filterExplicit
-                                    ? 'Explicit Off'
-                                    : 'Explicit On'}
-                            </div>
+                        <div className={styles.product}>
+                            <Icon icon="Star" size="1rem" />
+                            {user.product.charAt(0).toUpperCase() +
+                                user.product.slice(1)}
+                        </div>
+                        <div className={styles.product}>
+                            <Icon icon="Lock" size="1rem" />
+                            {user.filterExplicit
+                                ? 'Explicit Off'
+                                : 'Explicit On'}
                         </div>
                     </div>
-                </Panel>
-            </aside>
+                </div>
+            </div>
         </div>
     </Panel>
 );
