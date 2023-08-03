dcba7542774cc46fb300bd86d20e963ea4f6fb4e
Joseph Collicoat <jcollicoat@gmail.com>
2023-08-04 00:45:34 +1200

    Defaults to hooks

--- library/hooks/useMediaCommon.ts
+++ library/hooks/useMediaCommon.ts
@@ -1,11 +1,11 @@
 import { useMedia } from 'react-use';
 
 export const useMediaMobile = () => {
-    const isMobile = useMedia('(max-width: 479px)');
+    const isMobile = useMedia('(max-width: 479px)', false);
     return isMobile;
 };
 
 export const useMediaTiny = () => {
-    const isTiny = useMedia('(max-width: 349px)');
+    const isTiny = useMedia('(max-width: 349px)', false);
     return isTiny;
 };
