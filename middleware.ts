03546432abde987ed4af25c3330cd68b7137b1fd
Joseph Collicoat <jcollicoat@gmail.com>
2023-08-08 22:03:25 +1200

    middleware as auth guard

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,48 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-const ENV = process.env.NODE_ENV;
+const secret = process.env.NEXTAUTH_SECRET;
 
-const oneHour = new Date().getTime() + 3600000;
-
-export function middleware(request: NextRequest) {
-    if (ENV === 'development' && !request.cookies.has('spotify_token')) {
-        const spotify_token = request.headers.get('authorization');
-        if (spotify_token) {
-            // Set cookie for this request
-            request.cookies.set({
-                name: 'spotify_token',
-                value: spotify_token,
-            });
-
-            // Set cookie in response
-            const nextWithCookie = NextResponse.next({ request });
-            nextWithCookie.cookies.set({
-                name: 'spotify_token',
-                value: spotify_token,
-                expires: oneHour,
-                httpOnly: true,
-            });
-            return nextWithCookie;
-        }
+export async function middleware(request: NextRequest) {
+    const jwt = await getToken({ req: request, secret });
+    if (!jwt) {
+        return NextResponse.redirect(new URL('/', request.url));
     }
+
+    // if (!request.cookies.has('spotify_token')) {
+    //     const jwt = await refreshSpotifyToken(token);
+
+    //     if (typeof jwt.spotify_token !== 'string') {
+    //         throw new Error('Could not automatically refresh spotify_token');
+    //     }
+
+    //     // Set cookie for this request
+    //     request.cookies.set({
+    //         name: 'spotify_token',
+    //         value: jwt.spotify_token,
+    //     });
+
+    //     // Set cookie in response
+    //     const nextWithCookie = NextResponse.next({ request });
+    //     nextWithCookie.cookies.set({
+    //         name: 'spotify_token',
+    //         value: jwt.spotify_token,
+    //         expires: oneHour,
+    //         httpOnly: true,
+    //     });
+    //     return nextWithCookie;
+    // }
 
     return NextResponse.next({ request });
 }
 
 export const config = {
-    matcher: ['/api/((?!auth).*)'],
+    matcher: [
+        '/albums/(.*)',
+        '/artists/(.*)',
+        '/playlists/(.*)',
+        '/search',
+        '/tracks/(.*)',
+        '/user/(.*)',
+    ],
 };
