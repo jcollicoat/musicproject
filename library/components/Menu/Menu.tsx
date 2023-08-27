75f0c059a7198c15a1ee1be40e4740298160531b
Joseph Collicoat <jcollicoat@gmail.com>
2023-08-27 19:28:27 +1200

    Add focus trap

--- library/components/Menu/Menu.tsx
+++ library/components/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import classNames from 'classnames';
+import FocusTrap from 'focus-trap-react';
 import Image from 'next/image';
 import { FC, useMemo, useState } from 'react';
 import { Button, ButtonProps } from '@components/Button/Button';
@@ -21,36 +22,41 @@ export const Menu: FC<MenuProps> = ({ buttons, image, side }) => {
     const imageUrl = useMemo(() => image?.url, [image?.url]);
 
     return (
-        <div className={styles.wrapper}>
-            <button
-                aria-label="Menu"
-                className={classNames(styles.trigger, isOpen && styles.open)}
-                onClick={toggle}
-                type="button"
-            >
-                {imageUrl ? (
-                    <Image alt="Me" src={imageUrl} height={30} width={30} />
-                ) : (
-                    <Icon icon="Menu" isAlternate={isOpen} size="30px" />
-                )}
-            </button>
-            <menu
-                className={classNames(
-                    styles.menu,
-                    isOpen && styles.open,
-                    side && styles[side],
-                )}
-            >
-                {buttons.map((props) => (
-                    <li key={props.text}>
-                        <Button
-                            {...props}
-                            style="inMenu"
-                            tabIndex={isOpen ? 0 : -1}
-                        />
-                    </li>
-                ))}
-            </menu>
-        </div>
+        <FocusTrap active={isOpen}>
+            <div className={styles.wrapper}>
+                <button
+                    aria-label="Menu"
+                    className={classNames(
+                        styles.trigger,
+                        isOpen && styles.open,
+                    )}
+                    onClick={toggle}
+                    type="button"
+                >
+                    {imageUrl ? (
+                        <Image alt="Me" src={imageUrl} height={30} width={30} />
+                    ) : (
+                        <Icon icon="Menu" isAlternate={isOpen} size="30px" />
+                    )}
+                </button>
+                <menu
+                    className={classNames(
+                        styles.menu,
+                        isOpen && styles.open,
+                        side && styles[side],
+                    )}
+                >
+                    {buttons.map((props) => (
+                        <li key={props.text}>
+                            <Button
+                                {...props}
+                                style="inMenu"
+                                tabIndex={isOpen ? 0 : -1}
+                            />
+                        </li>
+                    ))}
+                </menu>
+            </div>
+        </FocusTrap>
     );
 };
