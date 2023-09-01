7603286d47eedc3e07022468845868e636714476
Joseph Collicoat <jcollicoat@gmail.com>
2023-09-01 21:48:45 +1200

    Slightly nicer track

--- library/components/Track/Track.tsx
+++ library/components/Track/Track.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { FC } from 'react';
 import { Track as TrackType } from '@music/types/tracks.types';
 import styles from './Track.module.scss';
@@ -8,8 +9,22 @@ interface Props {
 
 export const Track: FC<Props> = ({ track }) => {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.name}>{track.name}</div>
+        <div
+            className={styles.wrapper}
+            style={{ backgroundImage: `url(${track.album?.images[0].url})` }}
+        >
+            <div className={styles.content}>
+                {track.album && (
+                    <Image
+                        src={track.album.images[0].url}
+                        alt={track.album.name}
+                        height={60}
+                        width={60}
+                        className={styles.image}
+                    />
+                )}
+                <div className={styles.name}>{track.name}</div>
+            </div>
         </div>
     );
 };
