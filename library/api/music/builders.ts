543e403415ca69dc31a752a9a33e8ec9ce1e431f
Joseph Collicoat <jcollicoat@gmail.com>
2024-02-10 13:20:14 +1300

    Better image builder

--- library/api/music/builders.ts
+++ library/api/music/builders.ts
@@ -3,11 +3,27 @@ import { trackId, trackIds } from './builders/tracks.builders';
 import { user } from './builders/user.builders';
 import { Images } from './types';
 
-const images = (images: SpotifyImage[]): Images => ({
-    large: images[0]?.url,
-    medium: images[1]?.url,
-    small: images[2]?.url,
-});
+const images = (images: SpotifyImage[]): Images => {
+    if (images.length === 1) {
+        return {
+            large: images[0].url,
+            medium: images[0].url,
+            small: images[1].url,
+        };
+    } else if (images.length === 2) {
+        return {
+            large: images[0].url,
+            medium: images[1].url,
+            small: images[1].url,
+        };
+    } else {
+        return {
+            large: images[0].url,
+            medium: images[1].url,
+            small: images[2].url,
+        };
+    }
+};
 
 const builders = {
     images,
