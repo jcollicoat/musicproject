3f95cae73fb46156f4ee4b7414bc51d87a782e07
Joseph Collicoat <jcollicoat@gmail.com>
2024-02-10 14:14:52 +1300

    Move keys/modes

--- library/api/music/builders/audio.builders.ts
+++ library/api/music/builders/audio.builders.ts
@@ -1,27 +1,11 @@
-import { AudioFeatures } from '@music/types/audio.types';
+import {
+    AudioFeatures,
+    MusicalKeys,
+    MusicalModes,
+} from '@music/types/audio.types';
 import { SpotifyAudioFeatures } from '@spotify/types/audio.types';
 
-enum MusicalKeys {
-    'C',
-    'C#',
-    'D',
-    'D#',
-    'E',
-    'F',
-    'F#',
-    'G',
-    'G#',
-    'A',
-    'A#',
-    'B',
-}
-
-enum MusicalModes {
-    'Minor',
-    'Major',
-}
-
-const normalize = (feature: number) => Math.floor(feature * 100);
+const normalize = (audioFeature: number) => Math.floor(audioFeature * 100);
 
 const audioFeatures = (audioFeatures: SpotifyAudioFeatures): AudioFeatures => ({
     acousticness: normalize(audioFeatures.acousticness),
--- library/api/music/types/audio.types.ts
+++ library/api/music/types/audio.types.ts
@@ -1,3 +1,23 @@
+export enum MusicalKeys {
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'A#',
+    'B',
+}
+
+export enum MusicalModes {
+    'Minor',
+    'Major',
+}
+
 interface Interval {
     duration: number;
     start: number;
