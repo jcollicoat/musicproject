72fc8ae3eb21ef7b58df9f98f74f6d982d0d5466
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-08 19:16:32 +0100

    hours in time text

--- library/components/TimeText/TimeText.tsx
+++ library/components/TimeText/TimeText.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import styles from './TimeText.module.scss';
 
 interface Props {
     durationMs: number;
@@ -6,24 +7,28 @@ interface Props {
 }
 
 export const TimeText: FC<Props> = ({ durationMs, title = 'Duration' }) => {
-    const totalSeconds = Math.ceil(durationMs / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
+    const totalSeconds = Math.floor(durationMs / 1000);
+    const hours = Math.floor(totalSeconds / 60 / 60);
+    const minutes = Math.floor(totalSeconds / 60 - hours * 60);
     const seconds = totalSeconds % 60;
 
     return (
         <span
-            aria-label={`${title}: ${minutes} minutes, ${seconds} seconds`}
-            style={{ whiteSpace: 'nowrap' }}
+            aria-label={`${title}: ${
+                hours !== 0
+                    ? `${hours} ${hours === 1 ? 'hour' : 'hours'}, `
+                    : ''
+            }${minutes} minutes, ${seconds} seconds`}
+            className={styles.time}
         >
-            {minutes}
-            <span
-                style={{
-                    display: 'inline-block',
-                    transform: 'translateY(-0.1em)',
-                }}
-            >
-                :
-            </span>
+            {hours !== 0 && (
+                <>
+                    {hours}
+                    <span className={styles.separator}>:</span>
+                </>
+            )}
+            {hours !== 0 && minutes === 0 ? `0${minutes}` : minutes}
+            <span className={styles.separator}>:</span>
             {seconds < 10 ? `0${seconds}` : seconds}
         </span>
     );
