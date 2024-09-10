28e9e3278ce0e30671a0e29d823d94850993e2d3
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-10 01:43:52 +0100

    improve titleCase helper

--- library/api/helpers.ts
+++ library/api/helpers.ts
@@ -58,7 +58,7 @@ export const titleCase = (input: string) =>
     input
         .split(' ')
         .map((word) =>
-            word.length < 2 || word.includes('&')
+            word.length < 3 || word.includes('&')
                 ? word.toUpperCase()
                 : word.charAt(0).toUpperCase() + word.slice(1),
         )
