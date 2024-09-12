7587da5f42f5f06fb35ca24c02a44b9e4b07dc8f
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-12 01:16:06 +0100

    remove logger

--- app/tracks/[trackId]/page.tsx
+++ app/tracks/[trackId]/page.tsx
@@ -1,24 +1,20 @@
 import { AudioFeatures } from '@components/AudioFeature/AudioFeatures';
-import { ClientLogger } from '@components/ClientLogger/ClientLogger';
+
 import { Header } from '@components/Header/Header';
 import { Panel } from '@components/Panel/Panel';
-import { music } from '@music/api';
 import styles from './page.module.scss';
 
 export const metadata = {
     title: 'Track',
 };
 
-export default async function Page({
+export default function Page({
     params: { trackId },
 }: {
     params: { trackId: string };
 }) {
-    const audio = await music.audio.features(trackId);
-
     return (
         <>
-            <ClientLogger data={audio} />
             <Header trackId={trackId} />
             <main className={styles.main}>
                 <Panel
