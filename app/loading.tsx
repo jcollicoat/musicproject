94180b74b2cc96f6e92403614e8168a789305e08
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-18 19:32:57 +0100

    loading update

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,12 @@
 import { Shimmer } from 'Panels/Shimmer/Shimmer';
-import styles from './loading.module.scss';
+import './loading.scss';
 
 export default function Loading() {
     return (
-        <main className={styles.main}>
-            <Shimmer gridArea="one" shape="circle" />
-            <Shimmer gridArea="two" />
+        <main>
+            <Shimmer gridArea="one" height={210} />
+            <Shimmer gridArea="two" shape="circle" />
+            <Shimmer gridArea="three" />
         </main>
     );
 }
--- components/Panels/Shimmer/Shimmer.tsx
+++ components/Panels/Shimmer/Shimmer.tsx
@@ -1,19 +1,21 @@
 import classNames from 'classnames';
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 import { Panel, PanelProps } from 'Panels/Panel';
 import styles from './Shimmer.module.scss';
 
 interface ShimmerProps extends PanelProps {
+    height?: CSSProperties['height'];
     shape?: 'circle';
 }
 
-export const Shimmer: FC<ShimmerProps> = ({ shape, ...props }) => (
+export const Shimmer: FC<ShimmerProps> = ({ height, shape, ...props }) => (
     <Panel {...props}>
         <div
             className={classNames(
                 styles.shimmer,
                 shape === 'circle' && styles.circle,
             )}
+            style={{ height }}
         ></div>
     </Panel>
 );
